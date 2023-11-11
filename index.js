d0c4002f527d8debe637324b777a6b0b718675e0
nart <nart.janouk@gmail.com>
2023-11-10 21:32:22 -0500

    Add passing tests for the functions

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
-const gifts = ["teddy bear", "drone", "doll"];
-
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    debugger;
+function writeCards(names, event) {
+  let thankYouMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
-
-  return gifts;
+  return thankYouMessages;
 }
-
-wrapGifts(gifts);
-
-function writeCards(names, eventName) {
-    const thankYouMessages = [];
-  
-    for (let i = 0; i < names.length; i++) {
-      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
-      thankYouMessages.push(message);
-    }
-  
-    return thankYouMessages;
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
   }
-  
-  // Example usage:
-  const messages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
-  console.log(messages);
-  
-  function countDown(count) {
-    while (count >= 0) {
-      console.log(count);
-      count--;
-    }
-  }
-  
-  // Example usage:
-  countDown(10);
-  
+}
